fa5ca2147eaa6cec6a6ab6aa7b9a67ab44066f3c
GraphPaper2 <63701505+GraphPaper2@users.noreply.github.com>
2020-11-17 17:32:45 -0500

    made it give you prismarine shards / honeycomb based on your args

--- src/main/kotlin/world/cepi/example/ExampleExtension.kt
+++ src/main/kotlin/world/cepi/example/ExampleExtension.kt
@@ -1,11 +1,14 @@
 package world.cepi.example
 
+import net.minestom.server.MinecraftServer
 import net.minestom.server.extensions.Extension;
+import world.cepi.example.commands.EcoCommand
 
 class ExampleExtension : Extension() {
 
     override fun initialize() {
         logger.info("[ExampleExtension] has been enabled!")
+        MinecraftServer.getCommandManager().register(EcoCommand())
     }
 
     override fun terminate() {
--- src/main/kotlin/world/cepi/example/commands/EcoCommand.kt
+++ src/main/kotlin/world/cepi/example/commands/EcoCommand.kt
@@ -1,9 +1,9 @@
 package world.cepi.example.commands
 
+import net.minestom.server.MinecraftServer
 import net.minestom.server.command.CommandSender
 import net.minestom.server.command.builder.Arguments
 import net.minestom.server.command.builder.Command
-import net.minestom.server.command.builder.CommandExecutor
 import net.minestom.server.command.builder.arguments.Argument
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
@@ -12,8 +12,9 @@ import net.minestom.server.item.Material
 
 class EcoCommand : Command("eco") {
     init {
-        val amount = ArgumentType.Integer("amount").max(1).min(64)
-        val moneyType = ArgumentType.Word("type").from("crowns", "shards")
+        val amount = ArgumentType.Integer("amount")
+        val moneyType = ArgumentType.Word("moneytype").from("crowns", "shards")
+
         setCondition { sender, _ ->
             if (!sender.isPlayer) {
                 sender.sendMessage("Only players can use this command!")
@@ -23,8 +24,13 @@ class EcoCommand : Command("eco") {
 
         addSyntax({ sender: CommandSender, args: Arguments ->
             val player = sender as Player
-            player.inventory.addItemStack(ItemStack(Material.HONEYCOMB, args.getInteger("amount").toByte()))
-        }
+            if (args.getWord("moneytype") == "crowns") {
+                player.inventory.addItemStack(ItemStack(Material.HONEYCOMB, args.getInteger("amount").toByte()))
+            } else if (args.getWord("moneytype") == "shards") {
+                player.inventory.addItemStack(ItemStack(Material.PRISMARINE_SHARD, args.getInteger("amount").toByte()))
+            }
+
+        }, moneyType, amount
         )
     }
 }
