0b19a7d188ee4304e16384a3ce27239f33841836
GraphPaper2 <63701505+GraphPaper2@users.noreply.github.com>
2020-11-17 21:45:41 -0500

    Update EcoCommand.kt

--- src/main/kotlin/world/cepi/example/commands/EcoCommand.kt
+++ src/main/kotlin/world/cepi/example/commands/EcoCommand.kt
@@ -1,11 +1,7 @@
 package world.cepi.example.commands
 
-import net.minestom.server.MinecraftServer
 import net.minestom.server.chat.ChatColor
-import net.minestom.server.command.CommandSender
-import net.minestom.server.command.builder.Arguments
 import net.minestom.server.command.builder.Command
-import net.minestom.server.command.builder.arguments.Argument
 import net.minestom.server.command.builder.arguments.ArgumentType
 import net.minestom.server.entity.Player
 import net.minestom.server.item.ItemStack
@@ -13,34 +9,22 @@ import net.minestom.server.item.Material
 
 class EcoCommand : Command("eco") {
 
-    fun getP(name: String): Player? {
-
-       return MinecraftServer.getConnectionManager().onlinePlayers.firstOrNull { it.username.equals(name, true) }
-    }
-
     init {
+        setDefaultExecutor {sender, _ ->
+            sender.sendMessage("Usage: /eco <player> <reason>")
+        }
 
+        val amount = ArgumentType.Integer("amount").max(64).min(1)
+        val moneyType = ArgumentType.Word("type").from("crowns", "shards")
+        val targ = ArgumentType.Word("player")
 
-        val amount = ArgumentType.Integer("amount")
-        val moneyType = ArgumentType.Word("moneytype").from("crowns", "shards")
-        val playr = ArgumentType.String("player")
-        val action = ArgumentType.Word("action").from("give", "take")
-
-
-
-        addSyntax({ sender, args ->
+        addSyntax({sender, args ->
             val player = sender as Player
-            if (args.getWord("action") == "give") {
-                if (args.getWord("moneytype") == "crowns") {
-                    getP(args.getWord("player"))?.inventory?.addItemStack(ItemStack(Material.HONEYCOMB, args.getInteger("amount").toByte()))
-                } else if (args.getWord("moneytype") == "shards") {
-                    player.inventory.addItemStack(ItemStack(Material.PRISMARINE_SHARD, args.getInteger("amount").toByte()))
-                }
+            if (args.getWord("type") == "crowns") {
+                player.inventory.addItemStack(ItemStack(Material.HONEYCOMB, args.getInteger("amount").toByte()))
+                player.sendMessage("${ChatColor.BRIGHT_GREEN} + Gave Money")
             }
+        }, moneyType, amount)
 
-                
-
-        }, playr, moneyType, amount
-        )
     }
 }
