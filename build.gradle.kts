5677a12fb60a657d49c9275d3a48b004997253dc
LeoDog896 <LeoDog896@hotmail.com>
2021-03-20 20:04:26 -0400

    Bump Kepi

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
 
     // Add Kepi
-    compileOnly("com.github.Project-Cepi:Kepi:98950d8431")
+    compileOnly("com.github.Project-Cepi:Kepi:e124e9d1f8")
 }
 
 tasks.withType<Test> {
